2c205500118202b55469f01dbb3af76755a1590d
Chee <chhaysocheret73@gmail.com>
2023-01-13 00:38:04 +0700

    finish adding navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./scenes/navbar/index";
 import { SelectedPage } from "@/shared/types";
 
@@ -7,9 +7,31 @@ function App() {
     SelectedPage.Home
   );
 
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  /* A hook that is listening for the scroll event. */
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) {
+        setIsTopOfPage(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    // clean up function to remove the event listener
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
+
   return (
     <div className="app bg-gray-20 ">
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   );
 }
